b582b6b6473c1e5d47e820d7a5084fbfb021af89
Marcus H. <mhrejsa1@ivytech.edu>
2025-07-05 10:18:59 -0400

    Added Commets

--- Pharmacy_Portal/datahandler/views.py
+++ Pharmacy_Portal/datahandler/views.py
@@ -4,23 +4,23 @@ from .forms import create_medicine
 from django.shortcuts import redirect
 
 
-def pharmacyportal(request):
+def pharmacyportal(request): #Creates Phamarcy Portal
 
-    meds=Medicine.objects.values()
-    return render(request, 'pharmacyportal.html', {'meds': meds})
+    meds=Medicine.objects.values() #Gets all Medicines
+    return render(request, 'pharmacyportal.html', {'meds': meds}) #Passes Medicine to Phamarcy Portal HTML
 
 
-def selected_med(request, med_id):
+def selected_med(request, med_id): #When you click one of the medicines in the list, it will go to a dynamic url for that medicine.
 
     selected = Medicine.objects.filter(id=med_id).first()
     return render(request, 'selected_med.html', {'selected': selected})
 
-def delete_med(request, selected_id):
+def delete_med(request, selected_id): #Allows the user to delete a selected medicine in the dynamic URL
     selected = Medicine.objects.filter(id=selected_id).first()
     selected.delete()
     return redirect('pharmacyportal')
 
-def new_med(request):
+def new_med(request): #Allows the user to create new medicine, and will return to the pharmacy portal
 
     if request.method == "POST":
         form = create_medicine(request.POST)
