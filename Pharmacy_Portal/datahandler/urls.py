56775b01dc0bfc2a0812f23ec9436a6df8edc464
Marcus H. <mhrejsa1@ivytech.edu>
2025-07-12 14:13:34 -0400

    Added Login, Logout, User Validation, so that no one can look at the pharmacy portal without logging in, fixed some syntax.

--- Pharmacy_Portal/datahandler/urls.py
+++ Pharmacy_Portal/datahandler/urls.py
@@ -4,5 +4,6 @@ urlpatterns = [
     path('', views.pharmacyportal, name='pharmacyportal'),
     path('new_med/', views.new_med, name="new_med"),
     path('selected_med/<str:med_id>/', views.selected_med, name="selected_med"),
-    path('delete_med/<str:selected_id>/', views.delete_med, name='delete_med')
+    path('delete_med/<str:selected_id>/', views.delete_med, name='delete_med'),
+    path('logout', views.logout_user, name="logout")
 ]
--- Pharmacy_Portal/datahandler/views.py
+++ Pharmacy_Portal/datahandler/views.py
@@ -2,24 +2,35 @@ from django.shortcuts import render
 from .models import Medicine
 from .forms import create_medicine
 from django.shortcuts import redirect
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 
 
+
+@login_required(login_url='/')
 def pharmacyportal(request): #Creates Phamarcy Portal
 
     meds=Medicine.objects.values() #Gets all Medicines
     return render(request, 'pharmacyportal.html', {'meds': meds}) #Passes Medicine to Phamarcy Portal HTML
 
-
+@login_required(login_url='/')
 def selected_med(request, med_id): #When you click one of the medicines in the list, it will go to a dynamic url for that medicine.
 
     selected = Medicine.objects.filter(id=med_id).first()
     return render(request, 'selected_med.html', {'selected': selected})
 
+
+
+@login_required(login_url='/')
 def delete_med(request, selected_id): #Allows the user to delete a selected medicine in the dynamic URL
     selected = Medicine.objects.filter(id=selected_id).first()
     selected.delete()
     return redirect('pharmacyportal')
 
+
+
+
+@login_required(login_url='/')
 def new_med(request): #Allows the user to create new medicine, and will return to the pharmacy portal
 
     if request.method == "POST":
@@ -32,5 +43,8 @@ def new_med(request): #Allows the user to create new medicine, and will return t
         form = create_medicine()
     return render(request, 'new_med.html', {'form': form})
 
-    
+def logout_user(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect('/')    
 
