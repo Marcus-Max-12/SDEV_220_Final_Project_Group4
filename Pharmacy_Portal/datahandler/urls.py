15f8ec07aa025f29307c5237ea6ad23c2342fcc4
Petter <petterlewithoneeandtwot@gmail.com>
2025-07-16 15:11:03 -0500

    Added Client website functionality

--- Pharmacy_Portal/datahandler/urls.py
+++ Pharmacy_Portal/datahandler/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.pharmacyportal, name='pharmacyportal'),
+    path('new_client/', views.new_client, name="new_client"),
+    path('selected_client/<int:auto_increment_id>/', views.selected_client, name="selected_client"),
     path('new_med/', views.new_med, name="new_med"),
     path('selected_med/<str:med_id>/', views.selected_med, name="selected_med"),
     path('delete_med/<str:selected_id>/', views.delete_med, name='delete_med'),
--- Pharmacy_Portal/datahandler/views.py
+++ Pharmacy_Portal/datahandler/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from .models import Medicine
-from .forms import create_medicine
+from .models import Medicine, Client
+from .forms import create_medicine, create_client
 from django.shortcuts import redirect
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
@@ -11,13 +11,21 @@ from django.contrib.auth.decorators import login_required
 def pharmacyportal(request): #Creates Phamarcy Portal
 
     meds=Medicine.objects.values() #Gets all Medicines
-    return render(request, 'pharmacyportal.html', {'meds': meds}) #Passes Medicine to Phamarcy Portal HTML
+    clients=Client.objects.all() #Gets all Clients
+    return render(request, 'pharmacyportal.html', {'meds': meds, 'clients': clients}) #Passes Medicine to Phamarcy Portal HTML
+
 
 @login_required(login_url='/')
 def selected_med(request, med_id): #When you click one of the medicines in the list, it will go to a dynamic url for that medicine.
 
     selected = Medicine.objects.filter(id=med_id).first()
     return render(request, 'selected_med.html', {'selected': selected})
+
+@login_required(login_url='/')
+def selected_client(request, auto_increment_id): #When you click one of the clients in the list, it will go to a dynamic url for that medicine.
+
+    client = Client.objects.filter(id =auto_increment_id).first()
+    return render(request, 'selected_client.html', {'client': client})
 
 
 
@@ -27,7 +35,11 @@ def delete_med(request, selected_id): #Allows the user to delete a selected medi
     selected.delete()
     return redirect('pharmacyportal')
 
-
+@login_required(login_url='/')
+def delete_client(request, client_name): #Allows the user to delete a selected client in the dynamic URL
+    selected = Client.objects.filter(client_name=client_name).first()
+    selected.delete()
+    return redirect('pharmacyportal')
 
 
 @login_required(login_url='/')
@@ -42,6 +54,19 @@ def new_med(request): #Allows the user to create new medicine, and will return t
     else:
         form = create_medicine()
     return render(request, 'new_med.html', {'form': form})
+
+@login_required(login_url='/')
+def new_client(request): #Create new client and return to portal
+
+    if request.method == "POST":
+        form = create_client(request.POST)
+        if form.is_valid():
+            new = form.save(commit=False)
+            new.save()
+            return redirect('/pharmacyportal')
+    else:
+        form = create_client()
+    return render(request, 'new_client.html', {'form': form})
 
 def logout_user(request):
     if request.method == "POST":
